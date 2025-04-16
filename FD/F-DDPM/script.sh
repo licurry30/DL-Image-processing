5ee863454b570da34e224282f6d27041c31d91ee
licurry30 <2889425285@qq.com>
2025-04-16 12:20:41 +0800

    upload F-DDPM

---
+++ FD/F-DDPM/script.sh
@@ -0,0 +1,3 @@
+python fast_ddpm_main.py --config ldfd_linear.yml --dataset LDFDCT
+python fast_ddpm_main.py --config ldfd_linear.yml --dataset LDFDCT --sample --fid
+#修改相应的配置文件和dataset loader可适应三通道图像
